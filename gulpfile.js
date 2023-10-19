f2b393f47d73e562e8fdbbdb0a52dfc05c0899a3
Adithya Ajith <50308085+adithyajith925@users.noreply.github.com>
2023-10-19 13:23:18 -0400

    separation of scss into partial files + folder management

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles () {
-    return src('index.scss').pipe(sass()).pipe(dest('css'))
+    return src('prakasa/**/*.scss').pipe(sass()).pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['prakasa/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
